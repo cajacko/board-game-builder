197d8ad35b22424bad573e9ace88ba802a7ae376
Charlie Jackson <charlie@charliejackson.com>
2020-03-13 17:29:06 +0000

    feat(navigation): ✨ add basic navigation
    branch master

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,6 +7,9 @@ import {
   useRouteMatch,
   Switch
 } from "react-router-dom";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 import routes, { setApiKeyRoute } from "../config/routes";
 import NoPrint from "../components/NoPrint";
 
@@ -30,15 +33,27 @@ function Router() {
   return (
     <>
       <NoPrint>
-        {!!hasHistory && hasApiKey && (
-          <button onClick={() => history.goBack()}>Back</button>
-        )}
-
-        {route !== "/set-api-key" && (
-          <button onClick={() => history.push("/set-api-key")}>
-            Reset Api Key
-          </button>
-        )}
+        <Breadcrumbs aria-label="breadcrumb">
+          {hasApiKey && (
+            <Link color="inherit" href="/" onClick={() => history.push("/")}>
+              Home
+            </Link>
+          )}
+          {route !== "/set-api-key" && (
+            <Link
+              color="inherit"
+              href="/set-api-key"
+              onClick={() => history.push("/set-api-key")}
+            >
+              Reset Api Key
+            </Link>
+          )}
+          {!!hasHistory && hasApiKey && (
+            <Link color="inherit" href="/back" onClick={() => history.goBack()}>
+              Back
+            </Link>
+          )}
+        </Breadcrumbs>
       </NoPrint>
       <Switch>
         {routes.map(route => (
